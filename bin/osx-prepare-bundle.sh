9ccd5c8c3f9c62bb37fa6dc565535baf3c08f110
Andres Cabrera <mantaraya36@gmail.com>
2008-07-17 23:40:54 +0000

    Add OSX bundle script

---
+++ bin/osx-prepare-bundle.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+otool -L qutecsound.app/Contents/MacOS/qutecsound
+
+#mkdir qutecsound.app/Contents/Resources
+cp ../src/default.csd qutecsound.app/Contents/MacOS
+cp ../src/opcodes.xml qutecsound.app/Contents/MacOS
+
+mkdir qutecsound.app/Contents/Frameworks
+
+cp -R /Library/Frameworks/QtCore.framework qutecsound.app/Contents/Frameworks/
+cp -R /Library/Frameworks/QtGui.framework qutecsound.app/Contents/Frameworks/
+cp -R /Library/Frameworks/QtXml.framework qutecsound.app/Contents/Frameworks/
+
+install_name_tool -change qutecsound.app/Contents/Frameworks/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore qutecsound.app/Contents/MacOS/qutecsound 
+install_name_tool -change qutecsound.app/Contents/Frameworks/QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui qutecsound.app/Contents/MacOS/qutecsound
+install_name_tool -change qutecsound.app/Contents/Frameworks/QtXml.framework/Versions/4/QtXml @executable_path/../Frameworks/QtXml.framework/Versions/4/QtXml qutecsound.app/Contents/MacOS/qutecsound
+
+install_name_tool -id @executable_path/../Frameworks/QtCore.framework/Versions/4.0/QtCore qutecsound.app/Contents/Frameworks/QtCore.framework/Versions/4/QtCore
+install_name_tool -id @executable_path/../Frameworks/QtGui.framework/Versions/4.0/QtGui qutecsound.app/Contents/Frameworks/QtGui.framework/Versions/4/QtGui
+install_name_tool -id @executable_path/../Frameworks/QtXml.framework/Versions/4.0/QtXml qutecsound.app/Contents/Frameworks/QtXml.framework/Versions/4/QtXml
+
+install_name_tool -change  QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4.0/QtCore qutecsound.app/Contents/Frameworks/QtGui.framework/Versions/4.0/QtGui
+install_name_tool -change  QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/4.0/QtCore qutecsound.app/Contents/Frameworks/QtXml.framework/Versions/4.0/QtXml
+
+
