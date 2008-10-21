850b5d0c77055b6d7376aa63fad088372dbb9e8a
Andres Cabrera <mantaraya36@gmail.com>
2008-10-21 17:20:12 +0000

    Added script to generate source release

---
+++ srcrelease.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+if [ `uname` = "Darwin" ]; then
+   SVN_PATH="/opt/subversion/bin"
+fi
+
+echo Enter version number:
+read QUTECSOUND_VERSION
+
+$SVN_PATH/svn export https://qutecsound.svn.sourceforge.net/svnroot/qutecsound qutecsound-$QUTECSOUND_VERSION
+mv qutecsound-$QUTECSOUND_VERSION/trunk/qutecsound/* qutecsound-$QUTECSOUND_VERSION
+rm -R qutecsound-$QUTECSOUND_VERSION/trunk/
+tar -czf qutecsound-$QUTECSOUND_VERSION.tar.gz qutecsound-$QUTECSOUND_VERSION
+rm -R qutecsound-$QUTECSOUND_VERSION/
