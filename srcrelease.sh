3f099585536a5b2ebcce68f488650a55adf42ebd
Andres Cabrera <mantaraya36@gmail.com>
2010-03-15 16:03:03 +0000

    Fixed src release script to avoid including branches

--- srcrelease.sh
+++ srcrelease.sh
@@ -12,6 +12,7 @@ read QUTECSOUND_VERSION
 $SVN_PATH export https://qutecsound.svn.sourceforge.net/svnroot/qutecsound qutecsound-$QUTECSOUND_VERSION
 mv qutecsound-$QUTECSOUND_VERSION/trunk/qutecsound/* qutecsound-$QUTECSOUND_VERSION
 rm -R qutecsound-$QUTECSOUND_VERSION/trunk/
+rm -R qutecsound-$QUTECSOUND_VERSION/branches/
 #rm -R qutecsound-$QUTECSOUND_VERSION/doc/
 tar -czf qutecsound-$QUTECSOUND_VERSION-src.tar.gz qutecsound-$QUTECSOUND_VERSION
 rm -R qutecsound-$QUTECSOUND_VERSION/
