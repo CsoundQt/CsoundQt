288e0642a4b6de66aad7a34ad34ea5e24f5e7f7d
joachim heintz <jh@joachimheintz.de>
2012-08-16 17:51:11 +0200

    Some more and some extended Python scripts

---
+++ src/Scripts/Useful/rem_newline.py
@@ -0,0 +1,47 @@
+# removes newline markers in the selected section
+# joachim heintz 2012
+
+# -*- coding: utf-8 -*-
+
+import PythonQt.QtGui as pqt
+from string import replace
+
+      
+def remNewline():
+    selection = q.getSelectedText()
+    lines = selection.splitlines()
+    selNew = ""
+    for line in lines:
+        selNew = "%s %s" % (selNew, line.rstrip('\n'))
+    q.insertText(selNew.lstrip())
+    
+
+#info and render window
+w = pqt.QWidget() 
+w.setGeometry(200,50, 100,200)
+l = pqt.QGridLayout(w) 
+w.setLayout(l)
+w.setWindowTitle("Remove Newlines")
+
+renderButton = pqt.QPushButton("Remove!",w)
+text = pqt.QTextBrowser(w)
+l.addWidget(renderButton, 2, 0)
+l.addWidget(text, 1, 0)
+renderButton.connect("clicked()", remNewline)
+
+info = """Removes all newlines in the selected text."""
+text.setText(info)
+
+w.show()
+
+
+
+
+
+
+
+
+
+
+
+
