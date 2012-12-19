60b583b847483144bbec7a037578f1baf7e08d5a
Andres Cabrera <mantaraya36@gmail.com>
2012-12-18 20:02:46 -0800

    Improvements to srcrelease script

--- srcrelease.sh
+++ srcrelease.sh
@@ -4,6 +4,7 @@ echo Enter version number:
 read QUTECSOUND_VERSION
 
 git clone git://qutecsound.git.sourceforge.net/gitroot/qutecsound/qutecsound csoundqt-$QUTECSOUND_VERSION-src
+git checkout $QUTECSOUND_VERSION
 rm -Rf csoundqt-$QUTECSOUND_VERSION-src/.git
 tar -czf csoundqt-$QUTECSOUND_VERSION-src.tar.gz csoundqt-$QUTECSOUND_VERSION-src
 rm -Rf csoundqt-$QUTECSOUND_VERSION-src
