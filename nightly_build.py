807ee5504ba3e8e904e41fe6004f8736431a73a3
Andres Cabrera <andres@lenovo-W530>
2013-09-07 19:17:58 -0700

    Improvements to nightly build script

--- nightly_build.py
+++ nightly_build.py
@@ -1,16 +1,12 @@
 import os
+import shutil
 import sys
 from datetime import date
 
 # Set these global variables
-# Full path for Qt SDK libs, with '/' at the end
-qt_base_dir = '/opt/local/QtSDK/Desktop/Qt/474/gcc/'
-#qt_base_dir = '/opt/local/QtSDK/Desktop/Qt/4.8.1/gcc/'
-
-# if this is set to true, the framework installed and the build tools in PATH will be used.
-use_installed_framework = False
-if sys.platform == 'darwin':
-    use_installed_framework = True
+qt_base_dir = '~/Qt/5.1.1'
+qcs_source_path='~/src/qutecsound'
+qcs_build_prefix='~/src/'
 
 # -------------------------------------------------------------------
 # There should be no need to touch anything from here on to configure
@@ -19,17 +15,19 @@ if sys.platform == 'darwin':
 today = date.today().isoformat()
 build_dir = 'csoundqt-' + today
 
-configs = 'CONFIG+=release CONFIG+=buildDoubles CONFIG+=rtmidi CONFIG+=pythonqt '
-spec = '-spec max-g++ '
+configs = 'CONFIG+=release CONFIG+=buildDoubles CONFIG+=rtmidi CONFIG+=x86_64'
+#spec = '-spec max-g++ '
 
 qmake_bin = 'qmake -r '
 
-if not use_installed_framework:
-    qmake_bin = qt_base_dir + qmake_bin
+qmake_bin = qt_base_dir + '/' + qmake_bin
 
 os.system('git pull origin master')
 os.mkdir('../' + build_dir)
 os.cd('../' + build_dir)
 os.system(qmake_bin + spec + configs + ' ../csoundqt/qcs.pro')
-os.system('make -w')
-import ../bin/osx-package.py
+os.system('make -w -j7')
+os.system(qt_base_dir + '/' + 'macdeployqt ' + 'bin/CsoundQt-d-cs6.app/')
+shutil.copyfile('/usr/local/lib/libcsnd.6.0.dylib',  build_dir + '/bin/CsoundQt-d-cs6.app/Contents/Frameworks/')
+
+os.system('tar -czvf CsoundQt-nightly-%s.tar.gz CsoundQt-d-cs6.app &>/dev/null'%today)
