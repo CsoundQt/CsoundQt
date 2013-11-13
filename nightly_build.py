3b517ed6604e72fadd1a961f4d46aca86fbc8e35
Andres Cabrera <andres@lenovo-W530>
2013-11-13 15:48:34 -0800

    minor changes to nightly build script

--- nightly_build.py
+++ nightly_build.py
@@ -50,9 +50,9 @@ os.system(qt_base_dir + '/clang_64/bin/' + 'macdeployqt ' + 'bin/CsoundQt-d-cs6.
 sleep(10)
 os.chdir('bin')
 
-shutil.copyfile('/Library/Frameworks/CsoundLib64.framework/Versions/6.0/libcsnd.6.0.dylib',  'CsoundQt-d-cs6.app/Contents/Frameworks/libcsnd.6.0.dylib')
-
 os.system("install_name_tool -change /usr/local/lib/libsndfile.1.dylib @executable_path/../Frameworks/libsndfile.1.dylib CsoundQt-d-cs6.app/Contents/Frameworks/libcsnd.6.0.dylib")
+
+shutil.copyfile('/Library/Frameworks/CsoundLib64.framework/Versions/6.0/libcsnd.6.0.dylib',  'CsoundQt-d-cs6.app/Contents/Frameworks/libcsnd.6.0.dylib')
 
 os.system("install_name_tool -change CsoundLib64.framework/Versions/6.0/CsoundLib64 @executable_path/../Frameworks/CsoundLib64.framework/Versions/6.0/CsoundLib64 CsoundQt-d-cs6.app/Contents/Frameworks/libcsnd.6.0.dylib")
 
@@ -61,6 +61,13 @@ outname = 'CsoundQt-nightly-%s.tar.gz'%today
 os.system('tar -czvf %s CsoundQt-d-cs6.app &>/dev/null'%outname)
 # or: hdiutil create -format UDBZ -quiet -srcfolder YourApp.app YourAppArchive.dmg
 os.system('scp -i ~/.ssh/nightly %s %s@frs.sourceforge.net:/home/frs/project/qutecsound/CsoundQt/nightly-osx'%(outname,username))
+
+# With McCurdy Collection without CsoundLib (for release)
+outname = 'CsoundQt-forrelease-%s.tar.gz'%today
+os.system('rm -rf CsoundQt-d-cs6.app/Contents/Frameworks/CsoundLib64.framework'
+os.system('rm -rf CsoundQt-d-cs6.app/Contents/Frameworks/libcsnd.6.0.dylib')
+os.system('cp -rf ../../qutecsound/src/Examples/ CsoundQt-d-cs6.app/Contents/Re
+os.system('tar -czvf %s CsoundQt-d-cs6.app &>/dev/null'%outname)
 
 f.write("Done.\n")
 f.close()
