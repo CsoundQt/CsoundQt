c7c605a46c86f0aba4f2a4ddc83f43d8e3c5b6e0
Andres Cabrera <mantaraya36@gmail.com>
2014-11-07 21:10:07 -0800

    Put back stylesheet loading

--- src/qutecsound.cpp
+++ src/qutecsound.cpp
@@ -277,10 +277,10 @@ CsoundQt::CsoundQt(QStringList fileNames)
     //qDebug()<<"Max thread count: "<< QThreadPool::globalInstance()->maxThreadCount();
     QThreadPool::globalInstance()->setMaxThreadCount(MAX_THREAD_COUNT);
 
-//    QFile file(":/appstyle-white.css");
-//    file.open(QFile::ReadOnly);
-//    QString styleSheet = QLatin1String(file.readAll());
-//    qApp->setStyleSheet(styleSheet);
+	QFile file(":/appstyle-white.css");
+	file.open(QFile::ReadOnly);
+	QString styleSheet = QLatin1String(file.readAll());
+	qApp->setStyleSheet(styleSheet);
 }
 
 CsoundQt::~CsoundQt()
