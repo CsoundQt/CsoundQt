401d0b937b4564f70f60fe0d74d31fcd47ef22f7
gogins <michael.gogins@gmail.com>
2015-05-10 15:46:37 -0400

    Renamed these.

---
+++ src/csoundhtmlview.cpp
@@ -0,0 +1,84 @@
+#ifdef QCS_HTML5
+
+#include "documentpage.h"
+#include "csoundhtmlview.h"
+#include "ui_html5guidisplay.h"
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QWaitCondition>
+
+CsoundHtmlView::CsoundHtmlView(QWidget *parent) :
+    QDockWidget(parent),
+    ui(new Ui::Html5GuiDisplay),
+    documentPage(0),
+    webView(0)
+{
+    ui->setupUi(this);
+    webView = new QCefWebView(this);
+    setWidget(webView);
+    //webView->loadFromUrl(QUrl("http://csound.github.io/docs/manual/indexframes.html"));
+    webView->sizePolicy().setVerticalPolicy(QSizePolicy::Policy::Expanding);
+    layout()->setMargin(0);
+}
+
+CsoundHtmlView::~CsoundHtmlView()
+{
+    delete ui;
+}
+
+QString getElement(const QString &text, const QString &tag)
+{
+    QString element = text.section("<" + tag + ">", 1, 1);
+    element = element.section("</" + tag + ">", 0, 0);
+    return element;
+}
+
+/**
+ * @brief Html5GuiDisplay.play
+ * @param documentPage
+ *
+ * Save the <CsHtml5> element, if it exists,
+ * to filename xxx.csd.html, and load it into the CEF web view.
+ */
+void CsoundHtmlView::play(DocumentPage *documentPage_)
+{
+    documentPage = documentPage_;
+    qDebug() << "Html5GuiDisplay::play()...";
+    auto text = documentPage.load()->getFullText();
+    auto filename = documentPage.load()->getFileName();
+    QFile csdfile(filename);
+    csdfile.open(QIODevice::WriteOnly | QIODevice::Text);
+    QTextStream out(&csdfile);
+    out << text;
+    csdfile.close();
+    auto html = getElement(text, "CsHtml5");
+    if (html.size() > 0) {
+        QString htmlfilename = filename + ".html";
+        QFile htmlfile(htmlfilename);
+        htmlfile.open(QIODevice::WriteOnly | QIODevice::Text);
+        QTextStream out(&htmlfile);
+        out << html;
+        htmlfile.close();
+        webView->loadFromUrl(QUrl::fromLocalFile(htmlfilename));
+    } else {
+        webView->loadFromUrl(QUrl("about:blank"));
+    }
+    repaint();
+}
+
+void CsoundHtmlView::stop()
+{
+    documentPage = 0;
+    qDebug() << "Html5GuiDisplay::stop()...";
+}
+
+void CsoundHtmlView::loadFromUrl(const QUrl &url)
+{
+    qDebug() << "Html5GuiDisplay::loadFromUrl()...";
+    if(webView != 0) {
+        //webView->evaluateJavaScript("debugger;");
+        webView->loadFromUrl(url);
+    }
+}
+
+#endif
