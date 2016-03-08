c095f71ee21199be669fa57432348c27f2e051f1
tarmoj <tarmo@otsakool.edu.ee>
2016-03-08 09:45:46 +0200

    Revert "Install target fixes for packagers"

--- mime-types/add_csound_mimetypes.sh
+++ mime-types/add_csound_mimetypes.sh
@@ -1,10 +1,7 @@
 #!/bin/sh 
 
 #copy icon for csound mimetypes
-INSTALLDIR="$1"
-if [ -z "$INSTALLDIR" ] ; then
-	INSTALLDIR=/usr #~/.local # use the latter one for local install
-fi
+INSTALLDIR=/usr #~/.local # use the latter one for local install
 ICONDIR=$INSTALLDIR/share/icons/hicolor/128x128/mimetypes 
 mkdir -v -p $ICONDIR
 cp -v csound-light-128.png $ICONDIR/csound.png
@@ -14,8 +11,6 @@ MIMEDIR=$INSTALLDIR/share/mime # or /usr/share/mime
 DESTDIR=$MIMEDIR/packages 
 mkdir -v -p $DESTDIR # make if does not exist
 cp -v *.xml $DESTDIR
-if [ "$2" != "no-update" ] ; then
-	update-mime-database -V $MIMEDIR
-fi
+update-mime-database -V $MIMEDIR
 
 echo "You may need to log out and log in to make changes effective." 
