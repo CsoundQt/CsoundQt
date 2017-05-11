aa72f56ee289396c9011c32ece124d25c32ec5fe
Tarmo Johannes <tarmo@otsakool.edu.ee>
2017-05-11 21:29:00 +0300

    Actually this is correct fix for many-tables-crash.

--- src/widgetlayout.cpp
+++ src/widgetlayout.cpp
@@ -1198,7 +1198,8 @@ uintptr_t WidgetLayout::getCurveById(uintptr_t id)
 void WidgetLayout::updateCurve(WINDAT *windat)
 {
 	//  qDebug() << "WidgetLayout::updateCurve(WINDAT *windat) " << windat->windid;
-	if (++curveUpdateBufferCount < QCS_CURVE_BUFFER_SIZE) {
+	if (curveUpdateBufferCount+1 < QCS_CURVE_BUFFER_SIZE) {
+		curveUpdateBufferCount++;
 		curveUpdateBuffer[curveUpdateBufferCount] = *windat;
 	}
 }
