592272886d3d7c5dc6b266957165d150ba90b785
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2020-07-14 03:21:14 +0200

    bump version

--- src/types.h
+++ src/types.h
@@ -29,7 +29,7 @@
 #include <csound.h>
 
 
-#define QCS_VERSION "0.9.8.1"
+#define QCS_VERSION "0.9.9"
 
 // Time in milliseconds for widget and console messages updates
 #define QCS_QUEUETIMER_DEFAULT_TIME 50
