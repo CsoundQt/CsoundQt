bd142855038bdf262889e5cb17db976f25bd7124
Tarmo Johannes <tarmo@otsakool.edu.ee>
2020-09-05 17:57:23 +0300

    Version to 1.0.0-beta

--- src/types.h
+++ src/types.h
@@ -29,7 +29,7 @@
 #include <csound.h>
 
 
-#define QCS_VERSION "0.9.8.2"
+#define QCS_VERSION "1.0.0-beta"
 
 // Time in milliseconds for widget and console messages updates
 #define QCS_QUEUETIMER_DEFAULT_TIME 50
