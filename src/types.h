b7a6c89fecb38a45e320ad7e4ca886d421cf3e36
Tarmo Johannes <tarmo@otsakool.edu.ee>
2022-10-30 20:21:42 +0200

    Version to 1.1.1

--- src/types.h
+++ src/types.h
@@ -29,7 +29,7 @@
 #include <csound.h>
 
 
-#define QCS_VERSION "1.1.0"
+#define QCS_VERSION "1.1.1"
 
 // Time in milliseconds for widget and console messages updates
 #define QCS_QUEUETIMER_DEFAULT_TIME 50
