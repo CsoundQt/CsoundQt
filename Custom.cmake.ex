d39f0b7173d92b9033adf3d1806250fb473b99dd
tarmo <trmjhnns@gmail.com>
2024-09-25 04:17:40 +0300

    Added Custom.cmake template (initial).

---
+++ Custom.cmake.ex
@@ -0,0 +1,14 @@
+# CUSTOM PROPERTIES TO SET
+
+# Copy this file to Custom.cmake and make necessary change to
+
+
+#option(CSQT_BUILD_DEBUGGER_SUPPORT "Build Debugger Support" ON)
+#option(CSQT_BUILD_RECORDING_SUPPORT "Build recording support" ON)
+
+#? option(CSQT_BUILD_RTMIDI_SUPPORT "Build internal MIDI (rtmidi) support" ON)
+#? option(CSQT_BUILD_HTML_SUPPORT "Build html support" ON)
+
+
+#? CSOUND_LIBRARY_PATH
+#? CSOUND_INCLUDE_PATHS
