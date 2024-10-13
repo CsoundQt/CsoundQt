57415a9bef6ef526d37d8e9999b7eb312c7aca20
tarmo <trmjhnns@gmail.com>
2024-10-13 19:19:11 +0300

    Added support for custom Csound paths.

--- Custom.cmake.ex
+++ Custom.cmake.ex
@@ -1,14 +1,13 @@
 # CUSTOM PROPERTIES TO SET
 
-# Copy this file to Custom.cmake and make necessary change to
+# Copy this file to Custom.cmake, uncomment  and make changes as needed
 
 
-#option(CSQT_BUILD_DEBUGGER_SUPPORT "Build Debugger Support" ON)
-#option(CSQT_BUILD_RECORDING_SUPPORT "Build recording support" ON)
+#option(CSQT_BUILD_DEBUGGER_SUPPORT "Build Debugger Support" ON) # default is OFF
+#option(CSQT_BUILD_RECORDING_SUPPORT "Build recording support" OFF) # default is ON
+#option(CSQT_BUILD_RTMIDI_SUPPORT "Build internal MIDI (rtmidi) support" OFF) #default is ON
+#option(CSQT_BUILD_HTML_SUPPORT "Build html support" OFF) #default is ON
 
-#? option(CSQT_BUILD_RTMIDI_SUPPORT "Build internal MIDI (rtmidi) support" ON)
-#? option(CSQT_BUILD_HTML_SUPPORT "Build html support" ON)
-
-
-#? CSOUND_LIBRARY_PATH
-#? CSOUND_INCLUDE_PATHS
+#for custom paths:
+#set(CSOUND_INCLUDE_PATH "<your path>/include/csound/")
+#set(CSOUND_LIBRARY_PATH "<your path>/lib/")
