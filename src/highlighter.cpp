560bad426da74b0937382d84ae17deea53bf99ca
tarmo <trmjhnns@gmail.com>
2025-08-18 11:22:46 +0300

    Added highlighting for {{ }}

--- src/highlighter.cpp
+++ src/highlighter.cpp
@@ -811,7 +811,7 @@ void Highlighter::highlightCsoundBlock(const QString &line)
         index = rxmatch.capturedEnd();
     }
 
-    //last rules
+    //last rules  -- not used except for label
     for(auto rule: lastHighlightingRules) {
         index = 0;
         while((rxmatch = rule.pattern.match(text, index)).hasMatch()) {
@@ -821,6 +821,8 @@ void Highlighter::highlightCsoundBlock(const QString &line)
             index = rxmatch.capturedEnd();
         }
     }
+
+
 
     setCurrentBlockState(0);
 
@@ -851,6 +853,41 @@ void Highlighter::highlightCsoundBlock(const QString &line)
         rxmatch = commentStartExpression.match(text, startIndex + commentLength);
         startIndex = rxmatch.hasMatch() ? rxmatch.capturedStart() : -1;
     }
+
+    // Multiline {{ ... }} strings
+    if (previousBlockState() == 2) {
+        // We are continuing inside a multiline block
+        int endIdx = line.indexOf("}}");
+        if (endIdx == -1) {
+            setFormat(0, line.length(), quotationFormat);
+            setCurrentBlockState(2);  // still inside
+            return;                   // stop here
+        } else {
+            setFormat(0, endIdx + 2, quotationFormat);
+            setCurrentBlockState(0);  // closed
+            // continue parsing after endIdx if needed
+        }
+    }
+
+    // If we get here, we are not inside a multiline block
+    int startIdx = line.indexOf("{{");
+    while (startIdx >= 0) {
+        int endIdx = line.indexOf("}}", startIdx + 2);
+        int stringLength;
+        if (endIdx == -1) {
+            setCurrentBlockState(2);
+            stringLength = line.length() - startIdx;
+        } else {
+            stringLength = endIdx - startIdx + 2;
+        }
+        setFormat(startIdx, stringLength, quotationFormat);
+
+        if (endIdx == -1)
+            break;
+        else
+            startIdx = line.indexOf("{{", endIdx + 2);
+    }
+
 }
 
 
